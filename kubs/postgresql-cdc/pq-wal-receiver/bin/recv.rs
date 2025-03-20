a67db057f2e85f35d9bd20d5d85072966a780f47
taekwombo <88032359+taekwombo@users.noreply.github.com>
2025-03-20 15:22:12 +0100

    chore(kubs): libpq it is

--- kubs/postgresql-cdc/pq-wal-receiver/bin/recv.rs
+++
@@ -1,6 +0,0 @@
-use pq_wal_receiver::*;
-
-fn main() {
-    init();
-    println!("Hello, world!");
-}
--- kubs/postgresql-cdc/pq-wal-receiver/src/lib.rs
+++ kubs/postgresql-cdc/pq-wal-receiver/src/lib.rs
@@ -1,11 +1,1 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-
-pub fn init() {
-    unsafe {
-        _PG_init();
-    }
-}
+pub fn lib() {}

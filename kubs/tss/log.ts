28b3dc433cee45de644b7b5d013f843da7e1d52a
taekwombo <88032359+taekwombo@users.noreply.github.com>
2026-08-08 17:12:48 +0200

    chore(kubs/tss): sort out logs

--- kubs/tss/log.ts
+++ kubs/tss/log.ts
@@ -1,17 +1,31 @@
-/// <reference types="./global.d.ts" />
+// https://en.wikipedia.org/wiki/ANSI_escape_code#Fe_Escape_sequences
+const CSI = String.fromCodePoint(27) + '[';
+const RED       = (v: string) => CSI + '1m'  + v + CSI + '0m';
+const PURPLE    = (v: string) => CSI + '35m' + v + CSI + '0m';
+const YELLOW    = (v: string) => CSI + '33m' + v + CSI + '0m';
+const BLUE      = (v: string) => CSI + '34m' + v + CSI + '0m';
+
+const ENCODER = new TextEncoder();
+
+function writeMessage(message: string, args: any[]): void {
+    const text = args.reduce((acc, v) => acc + ' ' + Deno.inspect(v), message);
+    const encoded = ENCODER.encode(text + '\n');
+
+    Deno.stderr.writeSync(encoded);
+}
 
 export function debug(message: string, ...args: any[]): void {
-    console.log('%cDEBUG', 'color:purple', message, ...args);
+    writeMessage(PURPLE('DEBUG') + ' ' + message, args);
 }
 
 export function info(message: string, ...args: any[]): void {
-    console.info('%cINFO', 'color:blue', message, ...args);
+    writeMessage(BLUE('INFO') + ' ' + message, args);
 }
 
 export function warn(message: string, ...args: any[]): void {
-    console.info('%cWARNING', 'color:yellow', message, ...args);
+    writeMessage(YELLOW('WARNING') + ' ' + message, args);
 }
 
 export function error(message: string, ...args: any[]): void {
-    console.error('%cERROR', 'color:red', message, ...args);
+    writeMessage(RED('ERROR') + ' ' + message, args);
 }

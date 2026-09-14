3578421b42517d1e06ea3199a8d76057e5a4567c
taekwombo <88032359+taekwombo@users.noreply.github.com>
2026-09-14 15:12:39 +0200

    chore(kubs/otel): link traces parent -> child

--- kubs/otel/trayray/examples/8_otel_trace_links.rs
+++ kubs/otel/trayray/examples/8_otel_trace_links.rs
@@ -1,7 +1,3 @@
-/// [opentelemetry::Context] can be used to provide information between spans indirectly.
-///
-/// Information set by span can be accessed by descendant spans.
-
 use opentelemetry::trace::TraceContextExt;
 use tracing_opentelemetry::OpenTelemetrySpanExt;
 use tracing_subscriber::layer::SubscriberExt;
@@ -15,13 +11,12 @@ fn main() {
         .init();
 
     let span = tracing::info_span!("base_span_with_links");
-    let otel_span_context = span.context().span().span_context().clone();
     let _g = span.enter();
 
     tracing::info!("Entering linked span now.");
 
     tracing::info_span!(parent: None, "inner").in_scope(|| {
-        tracing::Span::current().add_link(otel_span_context);
+        span.add_link(tracing::Span::current().context().span().span_context().clone());
         tracing::info!("Hi from inner - we are linked with base span.");
     });
 
